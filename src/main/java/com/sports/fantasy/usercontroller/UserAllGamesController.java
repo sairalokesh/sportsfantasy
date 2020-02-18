1c389723ef8656f45bfd6394932a70cc6f85c771
Lokesh.G <lokesh.g@trivium-esolutions.com>
2020-02-18 12:00:36 +0530

    Initial Commit

--- src/main/java/com/sports/fantasy/usercontroller/UserAllGamesController.java
+++ src/main/java/com/sports/fantasy/usercontroller/UserAllGamesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.sports.fantasy.domain.Ranking;
@@ -39,6 +40,10 @@ public class UserAllGamesController {
   @Autowired
   private GameAmountService gameAmountService;
   
+  @ModelAttribute
+  public void admindashboardtitle(Model model) {
+    model.addAttribute("title", "usergameshistory");
+  }
 
   @GetMapping(value = "/game/{gametype}/upcoming")
   public String upcoming(@PathVariable String gametype, Principal principal, Model model) {
